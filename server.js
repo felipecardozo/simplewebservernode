1d0b12325e0ef37a83bed5cd325ff62f803a0e25
Felipe Cardozo <pypelyncar@hotmail.com>
2018-04-13 20:27:21 -0500

    updates new web server

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+//npm install express --save
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(5555);
 //npm install connect serve-static
-var connect = require('connect');
+/*var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname)).listen(8080, function(){
     console.log('Server running on 8080...');
-});
+});*/
